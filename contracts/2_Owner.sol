c9d04aeb709d70f5a21018dfdad54555d4171b51
Daoud-Hussain <daoudhussain302@gmail.com>
2022-07-28 20:11:03 +0500

    Writing web contracts for ethereum

---
+++ contracts/2_Owner.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.7.0 <0.9.0;
+import "hardhat/console.sol";
+
+contract Owner {
+
+    address private owner;
+
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+
+    modifier isOwner() {
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+
+    constructor() {
+        console.log("Owner contract deployed by:", msg.sender);
+        owner = msg.sender; 
+        emit OwnerSet(address(0), owner);
+    }
+
+
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+} 
