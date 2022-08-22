5f8682815fff0d2e834fd499508e5a45ea3b0788
Daoud-Hussain <daoudhussain302@gmail.com>
2022-08-22 14:50:10 +0500

    importing contracts

---
+++ contracts/37_Impost.sol
@@ -0,0 +1,37 @@
+//Foo.sol
+
+pragma solidity ^0.8.13;
+
+struct Point {
+    uint x;
+    uint y;
+}
+
+error Unauthorized(address caller);
+
+function add(uint x, uint y) pure returns (uint) {
+    return x + y;
+}
+
+contract Foo {
+    string public name = "Foo";
+}
+
+
+//Import.sol
+
+// import Foo.sol from current directory
+import "./Foo.sol";
+
+// import {symbol1 as alias, symbol2} from "filename";
+import {Unauthorized, add as func, Point} from "./Foo.sol";
+
+contract Import {
+    // Initialize Foo.sol
+    Foo public foo = new Foo();
+
+    // Test Foo.sol by getting it's name.
+    function getFooName() public view returns (string memory) {
+        return foo.name();
+    }
+}
